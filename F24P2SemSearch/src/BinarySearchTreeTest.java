24544ef239c079938d90d9246983481e0d600ee5
adamschantz <146872893+adamschantz@users.noreply.github.com>
2024-09-26 16:57:28 -0400

    test

--- F24P2SemSearch/src/BinarySearchTreeTest.java
+++ F24P2SemSearch/src/BinarySearchTreeTest.java
@@ -15,13 +15,17 @@ public class BinarySearchTreeTest extends TestCase
     private BinarySearchTree<Integer> search;
     private BSTNode<Integer> node;
     private Record<Integer> record;
+    private Seminar sem;
     
     /**
      * setUp method for the test class
      */
     public void setUp()
     {
-        record = new Record<Integer>(5, null);
+        String[] strings = new String[1];
+        //20, "jett", "today", 5, 10, 10, 50, strings, "test"
+        sem = new Seminar();
+        record = new Record<Integer>(5, sem);
         search = new BinarySearchTree<Integer>();
         node = new BSTNode<Integer>(record);
     }
@@ -32,7 +36,13 @@ public class BinarySearchTreeTest extends TestCase
     public void testInsert()
     {
         search.insert(record);
-        
+        System.out.println(record.getKey());
+        System.out.println(search.find(record).getRecord().getKey());
+        //System.out.println(record.getKey());
+        //System.out.println(search.find(record).getRecord().getKey());
+        System.out.println(record.getSem().id());
+        System.out.println(search.find(record).getRecord().getSem().id());
+        assertEquals(record.getSem(), search.find(record).getRecord().getSem());
         assertEquals(search.find(record), node);
     }
 
