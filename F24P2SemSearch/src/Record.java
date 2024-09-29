202aa7886fd103a0ddae539776e20a2c7b2fc16c
jett2 <jettmorrow@vt.edu>
2024-09-29 13:54:02 -0400

    woo

--- F24P2SemSearch/src/Record.java
+++ F24P2SemSearch/src/Record.java
@@ -11,27 +11,48 @@ public class Record<T extends Comparable<T>> {
     private T key;
     private Seminar seminar;
 
+    /**
+     * constructor for Record
+     * @param k key
+     * @param sem Seminar
+     */
     public Record(T k, Seminar sem) {
         key = k;
         seminar = sem;
     }
 
 
+    /**
+     * getter for key
+     * @return key
+     */
     public T getKey() {
         return key;
     }
 
 
+    /**
+     * getter for Seminar
+     * @return Seminar
+     */
     public Seminar getSem() {
         return seminar;
     }
 
 
+    /**
+     * setter for Key
+     * @param key key
+     */
     public void setKey(T key) {
         this.key = key;
     }
 
 
+    /**
+     * setter for Seminar
+     * @param sem Seminar
+     */
     public void setSem(Seminar sem) {
         seminar = sem;
     }
