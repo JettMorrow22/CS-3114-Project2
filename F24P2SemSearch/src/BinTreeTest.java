d310875a8f622a26ae2c975d85ba4ca5d73379c5
adamschantz <146872893+adamschantz@users.noreply.github.com>
2024-10-16 16:37:59 -0400

    tests

--- F24P2SemSearch/src/BinTreeTest.java
+++ F24P2SemSearch/src/BinTreeTest.java
@@ -201,7 +201,9 @@ public class BinTreeTest extends TestCase {
             1, keywords, "description");
         Seminar x2y1 = new Seminar(5, "title", "data", 5, (short)2, (short)1,
             1, keywords, "description");
-        Seminar x2y2 = new Seminar(4, "title", "data", 4, (short)1, (short)0,
+        Seminar x2y2 = new Seminar(4, "title", "data", 4, (short)2, (short)2,
+            1, keywords, "description");
+        Seminar x2y3 = new Seminar(4, "title", "data", 4, (short)1, (short)0,
             1, keywords, "description");
         
         assertEquals(1, bin.search(5, 5, 20, output));
@@ -213,8 +215,7 @@ public class BinTreeTest extends TestCase {
         assertEquals(5, b2.search(2, 2, 1, output));
         
         b2.insert(x2y2);
-        assertEquals(6, b2.search(2, 2, 1, output));
-
+        assertEquals(7, b2.search(2, 2, 1, output));
         
     }
 }
